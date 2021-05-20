35377b7a94d147143c4b6f72743578c6b469513e
husnul23 <fadhilah.acsl@gmail.com>
2021-05-20 13:34:19 +0700

    added url for detail Product

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
-from .views import ProductList
+from .views import ProductDetail, ProductList
 
 urlpatterns = [
     path('', ProductList.as_view()),
+    path('<int:pk>/', ProductDetail.as_view(), name='retrieve-product'),
 ]
