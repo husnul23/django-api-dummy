7f3b546ba4dd31096bbb72db5b234a532d139005
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2021-05-24 10:21:32 +0700

    fix

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
